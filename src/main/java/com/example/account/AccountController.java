c9a369d3c72b68b42348273e4ce1a75ad93be013
study-cho <dmcho.ex@gmail.com>
2023-06-22 10:53:00 +0900

    회원 가입 폼 서브밋 검증

--- src/main/java/com/example/account/AccountController.java
+++ src/main/java/com/example/account/AccountController.java
@@ -1,16 +1,40 @@
 package com.example.account;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
 //        model.addAttribute("signUpForm", new SignUpForm());
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if(errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        // TODO 회원 가입 처리
+        return "redirect:/";
     }
 }
