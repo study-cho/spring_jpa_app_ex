f45d516f92c4edbc1f97b7ee5d6db23112d0709a
study-cho <dmcho.ex@gmail.com>
2023-06-22 14:27:43 +0900

    회원 가입 테스트 및 리팩토링

--- src/main/java/com/example/account/AccountController.java
+++ src/main/java/com/example/account/AccountController.java
@@ -18,8 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -39,25 +38,9 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .nickname(signUpForm.getNickname())
-                .email(signUpForm.getEmail())
-                .password(signUpForm.getPassword()) // TODO encoding 해야 함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdateByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("스터디올래, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
-                + "&email=" + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
-
+        accountService.processNewAccount(signUpForm);
         return "redirect:/";
     }
+
+
 }
---
+++ src/main/java/com/example/account/AccountService.java
@@ -0,0 +1,44 @@
+package com.example.account;
+
+import com.example.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+
+    public void processNewAccount(SignUpForm signUpForm) {
+        Account newAccount = saveNewAccount(signUpForm);
+        newAccount.generateEmailCheckToken();
+        sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .nickname(signUpForm.getNickname())
+                .email(signUpForm.getEmail())
+                .password(signUpForm.getPassword()) // TODO encoding 해야 함
+                .studyCreatedByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdateByWeb(true)
+                .build();
+        return accountRepository.save(account);
+    }
+
+    private void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올래, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
+                + "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+    }
+
+}
